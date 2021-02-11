456ea277132e92e36b3b8fee1f2c33166bf8788b
AtomicProgramming <georgetimms2005@outlook.com>
2021-02-11 23:58:49 +0000

    Added txt file to store token

--- src/main/java/com/atomicprogramming/StatsBot/Bot.java
+++ src/main/java/com/atomicprogramming/StatsBot/Bot.java
@@ -6,13 +6,43 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Bot {
-    public static final String token = "";
     public static final Logger logger = LoggerFactory.getLogger(Bot.class);
-    public static void main(String[] args) {
+
+    public static String GetToken() {
         try {
-        JDA jda = JDABuilder.createDefault(token).build();
+            return Files.readString(Path.of("token.txt"));
+        } catch (IOException error) {
+            logger.error("Could not find token file!");
+            return null;
+        }
+    }
+
+    public static void CheckTokenFile() {
+        File file = new File("token.txt");
+        try {
+            if (file.createNewFile()) {
+                logger.info("Created token.txt file, please add the discord token and rerun the program!");
+                System.exit(0);
+            }
+        } catch (IOException error) {
+            logger.error(String.valueOf(error));
+        }
+    }
+
+    public static void main(String[] args) {
+        CheckTokenFile();
+        String token = GetToken();
+        if (token == null) {
+            System.exit(1);
+        }
+        try {
+            JDA jda = JDABuilder.createDefault(token).build();
         }
         catch (LoginException error) {
             logger.error("Invalid or non-existent token!");
